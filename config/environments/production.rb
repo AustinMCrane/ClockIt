2dd955a9df2c7fda11a2ea0012fea1724bf11cb5
Austin M Crane <amcrane1@wichita.edu>
2016-03-11 14:38:40 -0600

    tryign to fix mailer problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,14 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.default_url_options = { :host => 'ennovar-timekeeping.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local  = true
