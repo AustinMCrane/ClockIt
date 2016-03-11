bbe55b57527d0d4b87f19aedf24d7c4bb554219a
Austin M Crane <amcrane1@wichita.edu>
2016-03-11 13:26:03 -0600

    added pre redener to some of the jsx files

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,13 @@ module Timeclock
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.react.addons = true # defaults to false
+    config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
+    config.react.server_renderer_timeout    ||= 20 # seconds
+    config.react.server_renderer = React::ServerRendering::SprocketsRenderer
+    config.react.server_renderer_options = {
+      files: ["react-server.js", "components.js", "adminprojectslist.js.jsx","porjects.js.jsx", "task.js.jsx", "timekeeping.js.jsx", "timeoverview.js.jsx"], # files to load for prerendering
+      replay_console: true,                 # if true, console.* will be replayed client-side
+    }
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
